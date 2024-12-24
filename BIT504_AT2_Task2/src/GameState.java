4c3df143eab1e515a6456082a4dd18d577fa9650
OtHOWR <johnstone@myop.ac.nz>
2024-12-24 23:26:12 +1300

    Created GameState enum with states PLAYING, DRAW, CROSS_WON, and NOUGHT_WON

---
+++ BIT504_AT2_Task2/src/GameState.java
@@ -0,0 +1,8 @@
+// GameState enum
+
+public enum GameState {
+    PLAYING, // game currently is being played
+    DRAW, // The game ended in a draw
+    CROSS_WON, // Cross player won the game
+    NOUGHT_WON // Nought player won the game
+}
